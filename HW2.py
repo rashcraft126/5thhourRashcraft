c5cf9ac220a2d04562a94ee9672826152979ae08
Ryley Ashcraft <ra126319@student.wpsok.org>
2024-08-21 13:18:17 -0500

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,23 @@
+#Name: Ryley Ashcraft
+#Class: 5th hour
+#Assignment: HW2
+#Sources: https://stackoverflow.com/questions/33599306/how-to-insert-an-answer-into-python
+
+print("Hello world!")
+
+print("what is your name?")
+
+input(" ")
+
+print("Give me one number")
+
+x=input(" ")
+
+print("Give me another number")
+
+y=input(" ")
+
+sum= int(x) + int(y)
+print(sum)
+
+
