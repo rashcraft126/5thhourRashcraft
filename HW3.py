eb6594e924a2031fb7bc4f9d166d5b78ec9b111d
Ryley Ashcraft <ra126319@student.wpsok.org>
2024-08-29 12:38:21 -0500

    Added HW3

---
+++ HW3.py
@@ -0,0 +1,18 @@
+#Name: Ryley Ashcraft
+#Class: 5th hour
+#Assignment: HW3
+
+print("Hello World!")
+namelist = ["Ally", "Zachary", "Dom", "Ethan", "Bailey"]
+print(f"{namelist[1]}")
+namelist.append(input())
+print(f"{namelist[3]}")
+
+numblist = [2,3,7,8]
+numblist.insert(1,6)
+print(numblist)
+numblist.sort()
+print(numblist)
+sum = numblist[0] + numblist[1] + numblist[2]
+print(sum)
+
