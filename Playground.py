470c7ea6d48176658891ef862e04ec81d6d016a3
Ryley Ashcraft <ra126319@student.wpsok.org>
2024-09-06 13:16:51 -0500

    playground

---
+++ Playground.py
@@ -0,0 +1,29 @@
+#Name: Ryley Ashcraft
+#Class: 5th hour
+#Assignment:Playground
+
+print("Hello there, what is your name?")
+name=input()
+print("Wonderful name,", name)
+print("Now give me 3 other names")
+Name1=input()
+Name2=input()
+Name3=input()
+namelist=[Name1,Name2,Name3]
+print(namelist)
+print("Now lets do some math. Pick 5 numbers between 1-100")
+numb1=input()
+numb2=input()
+numb3=input()
+numb4=input()
+numb5=input()
+numblist= [numb1,numb2,numb3,numb4,numb5]
+print(numblist)
+x=numb1
+y=numb2
+a=numb3
+b=numb4
+c=numb5
+print("Theres your numbers. Now lets add", numb2, "and", numb5, ".")
+sum= y+c
+print(sum)
