49de08d6e7bfad5bf195cda692cbeb7f2defc1bd
Ryley Ashcraft <ra126319@student.wpsok.org>
2024-09-12 13:11:23 -0500

    HW4

---
+++ HW4.py
@@ -0,0 +1,26 @@
+#Name: Ryley Ashcraft
+#Class: 5th hour
+#Assignment: HW4
+
+print("Hello World!")
+Dogdict = { "breed":"Australian Shepard",
+            "color":"Grey,white & brown",
+            "life span":"13-15 years"}
+print(Dogdict["breed"])
+Dogdict.update({"size":"medium"})
+print(Dogdict)
+Dogdict.clear()
+print(Dogdict)
+fifthhour ={
+    "student1":{
+        "Name":"Zachary",
+        "Grade":12,
+        "phone":"iphone"
+    },
+    "student2":{
+        "Name":"Dominic",
+        "Grade":12,
+        "phone":"iphone",
+    }
+}
+print(fifthhour["student1"]["Name"],fifthhour["student2"]["Name"])
