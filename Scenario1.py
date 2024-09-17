43104d01f803688b24bd47e72a37a6ab52b27b42
Ryley Ashcraft <ra126319@student.wpsok.org>
2024-09-17 13:02:05 -0500

    Scenario1 modified and completed

--- Scenario1.py
+++ Scenario1.py
@@ -32,20 +32,23 @@ monsterdict= {
 
     }
 }
+print(monsterdict)
+print("Here is the monster dictionary before any applied changes.")
 
-newdamageA = input("Enter the Damage points for Domimomasourus")
-monsterdict["Domimomasourus"]["Damage"]=newdamageA
+newdamageA = input("Enter the new Damage points for Zachasourus")
+monsterdict["Zachasourus"]["Damage"]=newdamageA
 
-newdamageB = input("Enter the Damage points for Zachasourus")
-monsterdict["Zachasourus"]["Damage"]=newdamageB
+newdamageB = input("Enter the new Damage points for Domimomasourus")
+monsterdict["Domimomasourus"]["Damage"]=newdamageB
 
-newdamageC = input("Enter the Damage points for Gabasourus")
+newdamageC = input("Enter the new Damage points for Gabasourus")
 monsterdict["Gabasourus"]["Damage"]=newdamageC
 
-newdamageD = input("Enter the Damage points for Chimera")
+newdamageD = input("Enter the new Damage points for Chimera")
 monsterdict["Chimera"]["Damage"]=newdamageD
 
-newdamageE = input("Enter the Damage points for Allisorus")
+newdamageE = input("Enter the new Damage points for Allisorus")
 monsterdict["Allisorus"]["Damage"]=newdamageE
 
 print(monsterdict)
+print("Here is your new updated version of your monster dictionary.")
