e432ce4b7a1b7fd0c3402101b0cce939fbede488
Ryley Ashcraft <ra126319@student.wpsok.org>
2025-01-31 12:47:52 -0600

    HW19

---
+++ HW19.py
@@ -0,0 +1,15 @@
+#Name: Ryley Ashcraft
+#Class: 5th Hour
+#Assignment: HW19
+
+import random
+import time
+
+#1. Import all the functions from the HW15 file.
+from HW15 import hello_world, calculate_average, animal_list, print_number
+#2. Call the functions here and run HW19.
+hello_world()
+calculate_average(random.randint(1,15), random.randint(1,15), random.randint(1,15))
+animal_list("Lion", "Mouse", "Cat", "Dog", "Snake")
+print_number(10)
+#3. Delete all calls from HW15 and run HW19 again.
