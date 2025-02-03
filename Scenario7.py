c688627cf0bca1398bf9b3f0edbc0a34c00976eb
Ryley Ashcraft <ra126319@student.wpsok.org>
2025-02-03 13:17:21 -0600

    Scenario7

---
+++ Scenario7.py
@@ -0,0 +1,17 @@
+#Name: Ryley Ashcraft
+#Class: 5th Hour
+#Assignment: Scenario 7
+import random
+#Import all of Scenario 6 here
+from Scenario6 import stats
+listAverage = 0
+
+def final_average():
+    global listAverage
+    listAverage = average = sum(stats) / len(stats)
+    print(f"Average Stat: {average:.2f}")#Calculate the sum of the list by the length of the list here
+    return listAverage
+
+final_average()
+
+print(listAverage)
