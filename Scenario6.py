e3a345c4c4112d5f7eeaceac793cbde1adbd2d9e
Ryley Ashcraft <ra126319@student.wpsok.org>
2025-02-03 13:15:42 -0600

    Scenario6

---
+++ Scenario6.py
@@ -0,0 +1,34 @@
+#Name:Ryley Ashcraft
+#Class: 5th Hour
+#Assigment: Scenario 6
+import random
+
+#After an extended leave, the team lead for the RPG developer is back, and he wants to continue the project.
+#He wants to prototype the character creation model but first needs something that rolls stats for the characters.
+#He wants you to make a function that rolls 4 six-sided dice (d6), sorts them from highest to lowest, and then adds the
+#highest 3 together. He then wants you to add that result to a list outside the function. He wants you to run that function
+#5 more times (six times total) and print all six stats.
+
+#Once that is done, to ensure that the average of the statblock is fair (somewhere roughly between 12-13), he wants you
+#to plug it into a calculator (Scenario 7) and print the average.
+
+def roll_stat():
+    rolls = [random.randint(1, 6) for _ in range(4)]
+    rolls.sort(reverse=True)
+    return sum(rolls[:3])
+
+for i in range(6):
+    stats = []
+
+
+    for _ in range(6):
+        stats.append(roll_stat())
+
+    stats.sort(reverse=True)
+
+
+
+    print(f"Set {i + 1} Stats:", stats)
+
+    first_three_sum = sum(stats[:3])
+    print(f"Sum of the first three stats: {first_three_sum}")
